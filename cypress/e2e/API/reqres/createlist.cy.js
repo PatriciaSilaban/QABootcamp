fed521fc3da591c9b672e03450410fa7006decc2
PatriciaSilaban <silabanlahya@gmail.com>
2024-11-29 21:13:53 +0700

    update createlist

--- cypress/e2e/API/reqres/createlist.cy.js
+++ cypress/e2e/API/reqres/createlist.cy.js
@@ -2,9 +2,16 @@
 
 describe('Reqres API Testing', () => {
     it('POST API Create List', () => {
-        cy.request('POST', 'https://reqres.in/api/users').then((response) => {
+        const requestBody = {
+            name: 'Patricia Silaban',
+            job: 'leader'
+        };
+        cy.request('POST', 'https://reqres.in/api/users' , requestBody).then((response) => {
+            cy.log(JSON.stringify(response.body));
             expect(response.status).to.equal(201)
             expect(response.body).to.not.be.null
+            expect(response.body.name).to.equal(requestBody.name)
+            expect(response.body.job).to.equal(requestBody.job);
             })
     })
 })
